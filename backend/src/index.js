40b54773bda2ca34b387195937f237497017fad4
Moulya <projectalphadna@gmail.com>
2025-05-24 16:11:19 +0530

    WIP: Save local changes before pull --rebase

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,13 @@
-// src/index.js
-const app = require('./app');
+const app = require('./app'); // Import your Express app from app.js (or wherever it is)
+
+// Don't redeclare express app again here!
+// const express = require('express');
+// const cors = require('cors');
+
+// If you want to add cors here, do it on app, not a new express instance:
+const cors = require('cors');
+
+app.use(cors()); // Enable CORS on your imported app
 
 const port = 4000;
 
