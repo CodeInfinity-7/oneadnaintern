1d8b3da9776f1d985ce20b78eb64866feccc32fc
CodeInfinity-7 <146207268+CodeInfinity-7@users.noreply.github.com>
2026-07-16 15:46:16 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 
-const port = 4000;
+const port = process.env.PORT || 4000;
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
