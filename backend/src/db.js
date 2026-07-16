1c544ec454179ecb4f9073ae694c605bc2233014
CodeInfinity-7 <146207268+CodeInfinity-7@users.noreply.github.com>
2026-07-16 16:18:01 +0530

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,6 +1,8 @@
 const knex = require("knex");
 const config = require("../knexfile");
 
-const db = knex(config.development); // You can switch to .[process.env.NODE_ENV] later for environments
+const environment = process.env.NODE_ENV || "development";
+
+const db = knex(config[environment]);
 
 module.exports = db;
