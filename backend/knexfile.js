7f9ad199bbac8940e4b50283db5eeaab74278ffd
CodeInfinity-7 <146207268+CodeInfinity-7@users.noreply.github.com>
2026-07-16 16:28:07 +0530

    Update knexfile.js

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,48 +3,45 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
 module.exports = {
-
   development: {
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: 'postgres',     
-    password: 'Rmpm@2922',  
-    database: 'Onepayslip',
-    port: 2000
-  },
-  migrations: {
-    directory: './migrations'
-  }
-},
-
-
-  staging: {
-    client: 'postgresql',
+    client: "pg",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      host: "127.0.0.1",
+      user: "postgres",
+      password: "Rmpm@2922",
+      database: "Onepayslip",
+      port: 2000,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: "./migrations",
+    },
   },
 
-production: {
-  client: "pg",
-  connection: process.env.DATABASE_URL,
-  migrations: {
-    directory: "./migrations"
+  staging: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    migrations: {
+      directory: "./migrations",
+    },
   },
-  ssl: {
-    rejectUnauthorized: false
-  }
-}
-  
+
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+  },
 };
