0141ca70bccacea1cabe5488db383d93eb3c4110
CodeInfinity-7 <146207268+CodeInfinity-7@users.noreply.github.com>
2026-07-16 17:26:29 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:4000/:path*',
+        source: "/api/:path*",
+        destination: "https://oneadnaintern.onrender.com/:path*",
       },
     ];
   },
